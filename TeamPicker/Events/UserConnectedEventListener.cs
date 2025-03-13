a5c9c2704be285c8d1bcff337cdd11f818cb6aeb
liebesleid-eth <liebesleid99@gmail.com>
2025-03-12 21:05:07 -0700

    Added Lobby_Spawn to config

--- TeamPicker/Events/UserConnectedEventListener.cs
+++ TeamPicker/Events/UserConnectedEventListener.cs
@@ -13,6 +13,8 @@ using SDG.Unturned;
 using TeamPicker.UI;
 using OpenMod.Unturned.Users;
 using Microsoft.Extensions.Configuration;
+using UnityEngine;
+using TeamPicker.Models;
 
 namespace TeamPicker.Events
 {
@@ -46,8 +48,22 @@ namespace TeamPicker.Events
             if (user is UnturnedUser)
             {
                 await m_uIManager.StartSession<TeamPickSession>((UnturnedUser)@event.User, options, LifeTimeScope);
+
+                m_Configuration.GetSection("Active_Teams:Team_Two").Get<ulong>();
+                var lobbySpawn = m_Configuration.GetSection("Lobby_Spawn").Get<SpawnCoordinates>();
+                UnturnedUser player = (UnturnedUser)user;
+                if (lobbySpawn == null)
+                {
+                    Vector3 coords = new Vector3(0f, 0f, 0f);
+                    player.Player.Player.teleportToLocation(coords, 0f);
+                    await player.PrintMessageAsync("sent to default coords");
+                    return;
+                }
+                Vector3 spawn = new Vector3(lobbySpawn.X, lobbySpawn.Y, lobbySpawn.Z);
+                await player.PrintMessageAsync($"Coords: {lobbySpawn.X}, {lobbySpawn.Y}, {lobbySpawn.Z}");
+                player.Player.Player.teleportToLocation(spawn, 0f);
             }
-            
+
         }
     }
 }
